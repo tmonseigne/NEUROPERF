64560a8c8fa434ccd98e442b4b1b9f05f2bba7e5
Thibaut Monseigne <thibaut.monseigne@inria.fr>
2019-01-22 13:08:25 +0100

    :tada: Initial Commit

---
+++ src/ovp_main.cpp
@@ -0,0 +1,9 @@
+#include <openvibe/ov_all.h>
+#include "ovp_defines.h"
+// Boxes Includes
+//#include "boxes/ovpCBoxAlgorithmMyBox.h"
+
+OVP_Declare_Begin();
+	// Register boxes
+	//OVP_Declare_New(OpenViBEPlugins::MyPlugin::ovpCBoxAlgorithmMyBoxDesc);
+OVP_Declare_End();
